5b2271a7123243052b8bbb7e7fa290bb2dad9868
kovaldos <yakovaldos@yandex.ru>
2021-09-13 21:40:08 +0300

    updated my assembly of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,12 @@ let { src, dest, parallel } = require("gulp"),
   uglify = require("gulp-uglify-es").default,
   imagemin = require("gulp-imagemin"),
   webp = require("gulp-webp"),
-  webphtml = require("gulp-webp-html");
+  webphtml = require("gulp-webp-html"),
+  webpcss = require("gulp-webpcss"),
+  svgSprite = require("gulp-svg-sprite"),
+  ttf2woff = require("gulp-ttf2woff"),
+  ttf2woff2 = require("gulp-ttf2woff2"),
+  fonter = require("gulp-fonter");
 
 function browserSync(params) {
   browsersync.init({
@@ -75,6 +80,11 @@ function css() {
         cascade: true,
       })
     )
+    .pipe(
+      webpcss({
+        webpClass: '.webp',
+        noWebpClass: '.no-webp'
+    }))
     .pipe(dest(path.build.css))
     .pipe(clean_css())
     .pipe(
@@ -121,7 +131,33 @@ function images() {
     .pipe(browsersync.stream())
 }
 
-function watchFiles() {
+function fonts() {
+  src(path.src.fonts)
+  .pipe(ttf2woff())
+  .pipe(dest(path.build.fonts))
+  return src(path.src.fonts)
+  .pipe(ttf2woff2())
+  .pipe(dest(path.build.fonts))
+}
+
+gulp.task('otf2ttf', function () {
+  return gulp.src([sourceDir + '/fonts/*.otf'])
+})
+
+gulp.task('svgSprite', function () {
+  return gulp.src([sourceDir + '/svg/*.svg'])
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: "../icons/icons.svg", //sprite file name
+          // example: true
+        }
+      },
+    }))
+    .pipe(dest(path.build.img))
+})
+
+function watchFiles(params) {
   gulp.watch([path.watch.html], html);
   gulp.watch([path.watch.css], css);
   gulp.watch([path.watch.js], js);
@@ -132,9 +168,11 @@ function clean(params) {
   return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+
+exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
